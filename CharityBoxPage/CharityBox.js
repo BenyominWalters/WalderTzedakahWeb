b5fc85b102e036fab34ae7c605e4b654958a575c
Benyomin Walters <31862759+BenyominWalters@users.noreply.github.com>
2018-08-15 21:45:52 -0500

    Added ability to donate through custom paypal link

--- CharityBoxPage/CharityBox.js
+++ CharityBoxPage/CharityBox.js
@@ -6,6 +6,8 @@
 var boxTotal = 0;
 //var giveAmount = 0;
 
+var donationLink = ""
+
 function addValue(value) {
     //giveAmount = giveAmount + value;
     value = parseFloat(value, 10);
@@ -14,4 +16,34 @@ function addValue(value) {
     // The toFixed() function prevents javaScript from adding tons of decimal places to float
     // This way it displays as a two decimal place currency.
     document.getElementById("boxValue").innerHTML = "$" + boxTotal.toFixed(2); 
+}
+
+function resetBox(){
+    boxTotal = 0;
+    document.getElementById("boxValue").innerHTML = "$" + boxTotal.toFixed(2);
+}
+
+function buildDonationLink() {
+    let charityEmail = 'info@clhds.com';
+    let charityName = 'Cheder Lubavitch';
+    let donationInteger = parseInt(boxTotal); // Separates whole dollar value from boxTotal
+    let donationDecimal = parseInt(boxTotal % 1 * 100);
+    let donationAmount = donationInteger +'%2e'+ donationDecimal;
+    // Builds custom PayPal link. Its very long, so I broke it up onto multiple lines 
+    donationLink = 
+        'https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business='
+        +charityEmail
+        +'&lc=US&item_name=Donation+to+'
+        +charityName
+        +'&no_note=0&cn=&amount='
+        +donationAmount
+        +'&curency_code=USD&bn=PP-DonationsBF:btn_donateCC_LG.gif:NonHosted';
+}
+
+function donateBoxTotal() {
+    console.log("You Donated!");
+    buildDonationLink();
+    console.log(donationLink);
+    //window.open(donationLink, 'Donate', width=100,height=100);
+    resetBox();
 }
