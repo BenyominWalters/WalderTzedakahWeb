ab45b172232284b3921b42b8eef46f4189ed7728
Benyomin Walters <31862759+BenyominWalters@users.noreply.github.com>
2018-08-21 10:41:51 -0500

    Added give button with editible field. Button only displays if has value

--- CharityBoxPage/CharityBox.js
+++ CharityBoxPage/CharityBox.js
@@ -1,33 +1,45 @@
-/* Basic functinaly for Tzedakah Box.
+/* Basic functionality for Tzedakah Box.
     Clicking on value buttons should add appropriate value to total in box.
     Clicking donate will open paypal link, and clear value of box.
+    Add giveAmount to confirm how much you want to add?
 */
 
 var boxTotal = 0;
-//var giveAmount = 0;
+var giveAmount = 0; // Holds value of donation before adding to box to confirm.
 
 var donationLink = ""
 
 function addValue(value) {
-    //giveAmount = giveAmount + value;
-    value = parseInt(value);
+    value = parseInt(value); // Because value of button is string, converts to Int
     console.log(value);
-    boxTotal = boxTotal + value;
-    // The toFixed() function prevents javaScript from adding tons of decimal places to float
-    // This way it displays as a two decimal place currency.
-    document.getElementById("boxValue").innerHTML = "$" + (boxTotal/100).toFixed(2); 
+    giveAmount = giveAmount + value;
+    document.getElementById("giveAmount").value = + (giveAmount/100).toFixed(2);
+    // Uses toFixed() to trim extra float zeros to display as a two decimal place for currency.
+    document.getElementById("giveButton").className = "giveButton visible" // Makes button visible when has value to give
 }
+
+function giveToBox() {
+    boxTotal = boxTotal + giveAmount;
+    document.getElementById("boxValue").innerHTML = "$" + (boxTotal/100).toFixed(2);
+    // Uses toFixed() to trim extra float zeros to display as a two decimal place for currency.
+    
+    // Resets giveAmount
+    giveAmount = 0;
+    document.getElementById("giveAmount").value = + giveAmount.toFixed(2); // To fixed formats as $0.00, instead of just $0
+    document.getElementById("giveButton").className = "giveButton hidden" // Hides button when no value to give
+
+} 
 
 function resetBox(){
     boxTotal = 0;
-    document.getElementById("boxValue").innerHTML = "$" + (boxTotal/100).toFixed(2);
+    document.getElementById("boxValue").innerHTML = "$" + boxTotal.toFixed(2); // To fixed formats as $0.00, instead of just $0
 }
 
 function buildDonationLink() {
     let charityEmail = 'info@clhds.com';
     let charityName = 'Cheder Lubavitch';
-    let donationInteger = parseInt(Math.floor(boxTotal/100)); // Separates whole dollar value from boxTotal
-    let donationDecimal = boxTotal.toString().slice(-2); // Separates decimal values from boxTotal
+    let donationInteger = boxTotal.toString().slice(0, -2); // Separates whole dollar value from boxTotal--removes last two digits
+    let donationDecimal = boxTotal.toString().slice(-2); // Separates decimal values from boxTotal--keeps only last two digits
     let donationAmount = donationInteger +'%2e'+ donationDecimal;
     // Builds custom PayPal link. Its very long, so I broke it up onto multiple lines 
     donationLink = 
