39cd159aad987aa9dbd986ed0ed64fdcda4e812b
Benyomin Walters <31862759+BenyominWalters@users.noreply.github.com>
2018-09-13 17:05:37 -0500

    Changed settings page to modal

--- CharityBoxPage/CharityBox.js
+++ CharityBoxPage/CharityBox.js
@@ -11,9 +11,14 @@ var donationLink = ""
 
 var coinSound = new Audio('../Assets/Coin Sounds/zapsplat_foley_coin_drop_into_metal_collection_tin_006_21089.mp3');
 
-function loadSavedTotal() {
+function loadSaved() {
     boxTotal = parseInt(localStorage.boxTotal);
     document.getElementById("boxValue").innerHTML = "$" + (boxTotal/100).toFixed(2);
+
+    savedCharityName = localStorage.charityName;
+    document.getElementById("charityNameInput").value = savedCharityName;
+    savedCharityEmail = localStorage.charityEmail;
+    document.getElementById("charityEmailInput").value = savedCharityEmail;
 }
 
 function showGiveButton() {
@@ -93,4 +98,53 @@ function donateBoxTotal() {
     console.log(donationLink); // Turn on window to launch actual payment.
     window.open(donationLink, 'Donate', width=100,height=100);
     resetBox();
+}
+
+window.onload = function() {
+
+    // Get the settings modal
+    var modal = document.getElementById("settingsModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("settingsButton");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("closeButton")[0];
+
+    // Get the submit button
+    var submitButton = document.getElementById("settingsSubmitButton");
+
+    var savedCharityName = "Walder Education"
+    var savedCharityEmail = "teacherscenter@waldereducation.org"
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        console.log("click");
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+
+    submitButton.onclick = function() {
+        alert("sumbitted");
+        savedCharityName = document.getElementById("charityNameInput").value;
+        localStorage.setItem("charityName", savedCharityName);
+        savedCharityEmail = document.getElementById("charityEmailInput").value;
+        localStorage.setItem("charityEmail", savedCharityEmail);
+    }
+
+    if (localStorage) {
+        loadSaved()
+    }
+
 }
